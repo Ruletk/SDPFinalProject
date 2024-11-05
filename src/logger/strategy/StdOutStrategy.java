3cd2997fee6a72c90888cd3ad8ac47976880490d
Ruletk <damir.timergazin@gmail.com>
2024-11-06 04:51:18 +0500

    Added colors for StdOut output. Added getters for LogMessage

--- src/logger/strategy/StdOutStrategy.java
+++ src/logger/strategy/StdOutStrategy.java
@@ -3,8 +3,25 @@ package logger.strategy;
 import logger.LogMessage;
 
 public class StdOutStrategy implements Strategy {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+
     @Override
     public void log(LogMessage logMessage) {
-        System.out.println(logMessage.getFormattedMessage());
+        String formattedMessage = preProcessMessage(logMessage);
+        System.out.println(formattedMessage);
+    }
+
+    private String preProcessMessage(LogMessage logMessage) {
+        return switch (logMessage.getLevel()) {
+            case "INFO" -> ANSI_BLUE + logMessage.getFormattedMessage() + ANSI_RESET;
+            case "WARN" -> ANSI_YELLOW + logMessage.getFormattedMessage() + ANSI_RESET;
+            case "ERROR" -> ANSI_RED + logMessage.getFormattedMessage() + ANSI_RESET;
+            case "CRITICAL" -> ANSI_PURPLE + logMessage.getFormattedMessage() + ANSI_RESET;
+            default -> logMessage.getFormattedMessage();
+        };
     }
 }
