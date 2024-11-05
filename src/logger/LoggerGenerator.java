2fcfc6fa135486821d545de09c065c640cf02730
Adil Ormanov <>
2024-11-06 03:24:39 +0500

    Update LoggerGenerator for adding Adapter Pattern

--- src/logger/LoggerGenerator.java
+++ src/logger/LoggerGenerator.java
@@ -5,25 +5,27 @@ import logger.formatter.Formatter;
 import logger.strategy.Strategy;
 
 public class LoggerGenerator {
-    private static LoggerGenerator instance;
-    private final Formatter formatter;
-    private final Strategy[] strategies;
+    private static LoggerGenerator instance; // Singleton instance
+    private final Formatter formatter; // Formatter for log messages
+    private final Strategy[] strategies; // Array of logging strategies
 
-    private final LoggerFactory loggerFactory = new LoggerFactory();
+    private final LoggerFactory loggerFactory = new LoggerFactory(); // Logger factory instance
 
-    public LoggerGenerator(Formatter formatter, Strategy... strategies) {
-        if (instance != null) throw new IllegalStateException("LoggerGenerator has already been initialized");
-
-        this.strategies = strategies;
+    // Private constructor to prevent instantiation
+    private LoggerGenerator(Formatter formatter, Strategy... strategies) {
         this.formatter = formatter;
-        instance = this;
+        this.strategies = strategies;
     }
 
-    public static LoggerGenerator getInstance() {
-        if (instance == null) throw new IllegalStateException("LoggerGenerator has not been initialized yet");
+    // Method to get the singleton instance of LoggerGenerator
+    public static LoggerGenerator getInstance(Formatter formatter, Strategy... strategies) {
+        if (instance == null) {
+            instance = new LoggerGenerator(formatter, strategies);
+        }
         return instance;
     }
 
+    // Method to create a logger for the specified class
     public Logger getLogger(Class<?> clazz) {
         return loggerFactory.createLogger(clazz.getName(), formatter, strategies);
     }
