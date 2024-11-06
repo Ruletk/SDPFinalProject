179ea8357aacb2fadef51ad250768afc14f33c95
Ruletk <damir.timergazin@gmail.com>
2024-11-06 05:24:17 +0500

    Added Javadoc documentation to the code.

--- src/logger/LoggerConfiguration.java
+++ src/logger/LoggerConfiguration.java
@@ -3,6 +3,13 @@ package logger;
 import logger.formatter.Formatter;
 import logger.strategy.Strategy;
 
+/**
+ * LoggerConfiguration class is used to contain the formatter and strategies to log the messages.
+ * It's example of flyweight pattern.
+ * It is used by the Logger class to get the formatter and strategies to log the messages.
+ * It is created only once and passed to the Logger class.
+ * It is immutable.
+ */
 public class LoggerConfiguration {
     private final Formatter formatter;
     private final Strategy[] strategies;
--- src/logger/formatter/Formatter.java
+++ src/logger/formatter/Formatter.java
@@ -1,5 +1,18 @@
 package logger.formatter;
 
+/**
+ * Interface for implementations of log message formatters.
+ * Formatters are used to format log messages before they are logged to the output.
+ */
 public interface Formatter {
+    /**
+     * Formats the log message with the given level, timestamp, and logger name.
+     *
+     * @param message    String message to be formatted.
+     * @param level      Log level of the message.
+     * @param timestamp  Timestamp of the message.
+     * @param loggerName Name of the logger. Default is the class name.
+     * @return Formatted log message.
+     */
     String format(String message, String level, Long timestamp, String loggerName);
 }
